b3ea91cdb17738787bb41312bdfb9d20d45bc294
kana-web <kana.web4@gmail.com>
2025-04-14 16:18:11 +0900

    Delete portfolio directory

--- portfolio/index.js
+++
@@ -1,9 +0,0 @@
-function drawer() {
-  document.getElementById('line1').classList.toggle('line_1')
-  document.getElementById('line2').classList.toggle('line_2')
-  document.getElementById('line3').classList.toggle('line_3')
-  document.getElementById('navigation').classList.toggle('in')
-}
-document.getElementById('drawer-navigation').addEventListener('click',function(){
-  drawer(),false;
-});
